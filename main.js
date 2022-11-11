4b8703dc934a775ae41f40ff91ff300b12d7102c
slav-mirol <slavmirol@gmail.com>
2022-11-12 00:03:25 +0400

    modified:   main.js - добавлена функция перехода по нажатию кнопки на конкретный заголовок 	modified:   src/main.less - добавлены отступы в мобильную верстку 	deleted:    src/header copy.less 	deleted:    src/main copy.less 	deleted:    src/main.css 	modified:   src/utils/button.less - добавлены стили для различных состояний кнопки

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import 'normalize.css';
 import './index.less';
+
+const $button = document.querySelector('.button_scroll');
+const $topic = document.querySelector('.title_for_btn');
+
+$button.addEventListener('click', (e) => {
+  $topic.scrollIntoView({
+    block: 'start',
+    behavior: 'smooth',
+  });
+});
